965602ecf24354edff257e430928b362006d381a
Eduardo Vargas <eduardo.a.vargas@gmail.com>
2019-06-23 21:28:41 -0300

    inicio

---
+++ config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter', // Site title.
+  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml',
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleTagManagerID: '', // GTM tracking ID.
+  disqusShortname: 'gatsby-starter', // Disqus shortname.
+  userName: 'Eduardo Vargas',
+  userTwitter: 'eduardoavargas',
+  userLocation: 'Navegantes - SC - Brasil',
+  userDescription: '',
+  copyright: 'Copyright © Gatsby Starter 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#ffffff', // Used for setting manifest background color.
+}
